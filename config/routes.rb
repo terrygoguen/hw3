b7aff7683748565ff8f8a2969c7875136df0f7da
terrygoguen <terry.goguen@kellogg.northwestern.edu>
2023-02-16 00:57:00 +0000

    figured out routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # get("/", { :controller => "articles", :action => "index" })
+  resources "posts"
+  resources "place"
 end
