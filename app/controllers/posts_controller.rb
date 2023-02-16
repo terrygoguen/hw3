8bb7649023568993c3c721728d24606740b8fc04
terrygoguen <terry.goguen@kellogg.northwestern.edu>
2023-02-16 00:48:07 +0000

    adding controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+
+    def index
+        @posts = Post.all
+      end
+    
+      def new
+        @post = Post.new
+      end
+    
+      def create
+        @post = Post.new
+        @post["title"] = params["post"]["title"]
+        @post["description"] = params["post"]["description"]
+        @post.save
+        redirect_to "/posts"
+      end    
+    
+end
+
+
+  # create_table "posts", force: :cascade do |t|
+    #     t.string "title"
+    #     t.text "description"
+    #     t.date "posted_on"
+    #     t.integer "place_id"
+    #     t.datetime "created_at", null: false
+    #     t.datetime "updated_at", null: false
+    #   end
+    
+    # end
