a48f5e796fe3069842153c53a361fc37c23d339b
terrygoguen <terry.goguen@kellogg.northwestern.edu>
2023-02-17 02:10:02 +0000

    DoD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,16 @@ class PostsController < ApplicationController
     
       def new
         @post = Post.new
+        @place = Place.find_by({ "id" => params["place_id"] })
+        @post["place_id"] = @place["id"]
       end
     
       def create
         @post = Post.new
         @post["title"] = params["post"]["title"]
         @post["description"] = params["post"]["description"]
+        @post["posted_on"] = params["post"]["posted_on"]
+        @post["place_id"] = params["post"]["place_id"]
         @post.save
         redirect_to "/places"
       end    
